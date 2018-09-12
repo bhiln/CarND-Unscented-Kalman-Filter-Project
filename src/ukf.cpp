ae49d4ccd383e3f401b0a4e10da0f36c00fd713a
Brian Hilnbrand <brhiln@gmail.com>
2018-09-11 21:14:03 -0700

    Add: x_ and P_ initialization

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 3;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 3;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -56,7 +56,7 @@ UKF::UKF() {
   */
   is_initialized_ = false;
 
-  x_ = VectorXd(4);
+  P_ = MatrixXd::Identity(5, 5);
 }
 
 UKF::~UKF() {}
@@ -79,8 +79,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   if (!is_initialized_)
   {
     // first measurement
-    cout << "EKF: " << endl;
-    x_ << 1, 1, 1, 1;
+    x_ << 1, 1, 1, 1, 1;
 
     if (meas_package.sensor_type_ == MeasurementPackage::RADAR)
     {
@@ -92,21 +91,42 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
       float x = mag* cos(ang);
       float y = mag* sin(ang);
 
-      x_ << x, y, 0, 0;
+      x_ << x, y, 0, 0, 0;
     }
     else if (meas_package.sensor_type_ == MeasurementPackage::LASER)
     {
       /**
       Initialize state.
       */
-      x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0, 0;
+      x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0, 0, 0;
     }
+
+    //set state dimension
+    n_x_ = 5;
+
+    //set augmented dimension
+    n_aug_ = 7;
+
+    //define spreading parameter
+    lambda_ = 3 - n_aug_;
 
     time_us_ = meas_package.timestamp_;
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
+  }
+
+  //compute the time elapsed between the current and previous measurements
+	float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;	//dt - expressed in seconds
+	time_us_ = meas_package.timestamp_;
+
+  //for efficiency, if there is no change in time since the last sensor measurement
+  //then the prediction will be the same as the last prediction, and therefore no need
+  //to re-evaluate the prediction.
+  if (dt > 0){
+    //create sigma point matrix
+    MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
   }
 }
 
